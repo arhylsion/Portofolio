231a016c29210bcd2d4ee3ab64d1733dc39b914a
Athallah-Rikza <athallahrikza2@gmail.com>
2025-01-19 23:11:26 +0700

    tahap awal

---
+++ script.js
@@ -0,0 +1,12 @@
+
+  AOS.init({offset:0});
+      
+  // Navbar dropdown hamburger
+    function hamburg(){
+      const navbar = document.querySelector(".dropdown")
+      avbar.style.transform = "translateY(0px)"
+    }
+    function cancel(){
+      const navbar = document.querySelector(".dropdown")
+      navbar.style.transform = "translateY(-500px)"
+    }
